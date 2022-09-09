b93b7e7f47750ad36ebb385335fc36332b1bf519
michgur <michael@thegur.net>
2022-09-10 00:14:04 +0300

    updated import

--- src/classes.ts
+++ src/classes.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import parseConversation from './parsing';
+import parseConversation from './parsing/conversation';
 import type { OutputFields } from './skills';
 
 export interface Skill {
