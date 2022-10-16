9fb7681bd0e0cea667fda316ae9b6818f02eae46
michgur <michael@thegur.net>
2022-10-16 10:31:33 +0300

    added srt tests

--- test/parsing.ts
+++ test/parsing.ts
@@ -1,9 +1,13 @@
 /* eslint-disable no-unused-expressions */
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import chai from 'chai';
+import chaiFs from 'chai-fs';
 import { conversationParsingTests } from './constants.json';
 import transcriptionOutput from './transcriptionOutput.json';
 import oneai from './testClient';
+import { OneAIError } from '../src/errors';
+
+chai.use(chaiFs);
+const { expect } = chai;
 
 describe('parseConversation', () => {
   conversationParsingTests.forEach((test, index) => {
@@ -29,8 +33,28 @@ describe('parseConversation', () => {
 });
 
 describe('subtitle', () => {
-  it('srt', () => {
-    const parsed = oneai.parsing.toSRT(transcriptionOutput as any, { maxLengthCharacters: 30 });
-    console.log(parsed);
+  describe('srt', () => {
+    it('parse output', () => {
+      const parsed = oneai.parsing.toSRT(transcriptionOutput as any, { maxLengthCharacters: 30 });
+      expect('./test/testSRT.srt').to.be.a.file().with.content(parsed);
+    });
+
+    it('errors', () => {
+      const testObject: any = {
+        text: transcriptionOutput.text,
+        transcription: undefined,
+      };
+
+      expect(() => oneai.parsing.toSRT(testObject)).to.throw(OneAIError);
+      testObject.transcription = {
+        text: transcriptionOutput.transcription.text,
+        words: undefined,
+        sentences: undefined,
+      };
+
+      expect(() => oneai.parsing.toSRT(testObject)).to.throw(OneAIError);
+      testObject.transcription.words = transcriptionOutput.transcription.words;
+      expect(() => oneai.parsing.toSRT(testObject)).to.throw(OneAIError);
+    });
   });
 });
