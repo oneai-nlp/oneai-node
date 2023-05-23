e1a026c7754a1133433478c5fc725d21d165b73d
michgur <michael@thegur.net>
2023-05-23 17:53:25 +0300

    removed log

--- src/api/client.ts
+++ src/api/client.ts
@@ -117,7 +117,6 @@ export class ApiClientAxios implements HttpApiClient {
         maxContentLength: Infinity,
       });
     } catch (error) {
-      console.log(error);
       throw buildError(error);
     }
   }
